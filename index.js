9595ccd5a7d83226bce2fa9970938f5e3a893f1a
Rodrigo <rodrigo2219dg@gmail.com>
2020-06-11 13:39:04 -0300

    DB now syncs properly

--- index.js
+++ index.js
@@ -1,207 +1,235 @@
 const fs = require('fs');
+const axios = require('axios');
 const cheerio = require('cheerio');
-const axios = require('axios').default;
 
-function updateCases() {
-    return new Promise((resolve, reject) => {
-        const cases = {
+const COLOR = {
+    RED: '\x1b[31m%s\x1b[0m',
+    GREEN: '\x1b[32m%s\x1b[0m',
+    CYAN: '\x1b[36m%s\x1b[0m'
+};
 
-        };
-
-        axios.get('https://csgostash.com/containers/skin-cases').then(res => {
-            const $ = cheerio.load(res.data);
-
-            // creates an object with all the cases names and links
-            $('.well.result-box.nomargin') // get all cases
-                .not($('.abbu-body-resultbox').parent()) // removes ads
-                .each((i, el) => {
-                    cases[$(el).find('h4').text()] = $(el).find('a').attr('href');
-                });
-
-            // writes in disk as a json
-            fs.writeFile("./data/cases.json", JSON.stringify(cases), (err) => {
-                if (err) {
-                    reject(null);
-                    return console.warn('\x1b[31m%s\x1b[0m', "Erro ao atualizar a lista de caixas!\n", err);
-                }
-
-                console.warn("\x1b[32m%s\x1b[0m", "Caixas atualizadas com sucesso!");
-                resolve(cases);
-
-            });
-
-        }).catch(res => {
-            reject(null);
-            console.warn('\x1b[31m%s\x1b[0m', "Erro ao pegar informacao das caixas!\n", res);
-
-        });
-    });
-
-}
-
-function updateCaseSkins(caseName, caseLink) {
-    return new Promise((resolve, reject) => {
-        const skins = {
-
-        };
-
-        axios.get(caseLink).then(res => {
-            const $ = cheerio.load(res.data);
-
-            $('.well.result-box.nomargin') // gets all items
-                .not($('.quality.color-rare-item').parent()) // remove knives
-                .not($('.abbu-body-resultbox').parent()) // remove ads
-                .each((i, el) => {
-                    if (!skins[$(el).find('.quality').attr('class').slice(14, )])
-                        skins[$(el).find('.quality').attr('class').slice(14, )] = {}
-                    skins[$(el).find('.quality').attr('class').slice(14, )][$(el).find('h3').text()] = $(el).children('a').not('.nounderline').attr('href');
-                });
-
-            // writes json in disk
-            fs.writeFile(`./data/${caseName}.json`, JSON.stringify(skins), err => {
-                if (err) {
-                    reject(null);
-                    return console.warn('\x1b[31m%s\x1b[0m', "Erro ao atualizar a lista de caixas!\n", err);
-                }
-
-                console.warn("\x1b[32m%s\x1b[0m", `${caseName} atualizada!`);
-                resolve(skins);
-
-            });
-
-        }).catch(res => {
-            reject(null);
-            console.warn('\x1b[31m%s\x1b[0m', "Erro ao pegar informacao da caixa!\n", res);
-
-        });
-
-    });
-
-}
-
-function delay(ms){
-    return new Promise(resolve=>{
-        setTimeout(_=>resolve(), ms);
-    });
-}
-
-async function queryPrice(itemName) {
-    let link = `https://steamcommunity.com/market/priceoverview/?appid=730&currency=7&market_hash_name=${itemName}`;
-
-    return await axios.get(link).then(async res => {
-        if (!res.data.success)
-            return null;
-
-        const price = Number.parseFloat(res.data.median_price.slice(3, ).replace(',', '.'));
-        console.warn(`${itemName}: R$${price}`);
-        return await delay(3000).then(_=>{
-            return price;
-        });
-
-    }).catch(async res => {
-        console.warn('\x1b[31m%s\x1b[0m', `Erro preco ${itemName}!`);
-        return await delay(10000).then(_=>{
-            return queryPrice(itemName);
-        });
-
+function delay(ms) {
+    return new Promise(resolve => {
+        setTimeout(_ => resolve(), ms);
     });
 }
 
 function updateSkin(skinName, skinLink) {
     return new Promise((resolve, reject) => {
         const skin = {
-
+            name: skinName,
+            link: skinLink,
+            minWear: 0.0,
+            maxWear: 0.0,
+            'stat-track': {},
+            normal: {}
         };
 
-        const floats = {
-            "Factory New": 0.07,
-            "Minimal Wear": 0.15,
-            "Field-Tested": 0.38,
-            "Well-Worn": 0.45,
-            "Battle-Scarred": 1
-        }
-        let [fn, mw, ft, ww, bs] = ["Factory New", "Minimal Wear", "Field-Tested", "Well-Worn", "Battle-Scarred"];
-
-        axios.get(skinLink).then(async res => {
-            console.warn('\x1b[36m%s\x1b[0m', `${skinName}:`);
+        axios.get(skinLink, {timeout: 2000}).then(res => {
             const $ = cheerio.load(res.data);
 
             let wears = $('.marker-value.cursor-default').toArray();
             skin.minWear = Number.parseFloat($(wears[0]).text());
             skin.maxWear = Number.parseFloat($(wears[1]).text());
 
-            if (skin.minWear < floats[fn])
-                skin[fn] = await queryPrice(`${skinName} (${fn})`);
+            $('tbody').children().each((i, el) => {
+                // forEach table-row
+                let quality = $(el).children().first().text().replace(/\n/gi, '');
+                let isST = quality.includes('StatTrak');
+                let price = Number.parseFloat($(el).children().eq(3).text().replace(/\.|R\$ |\n/gi, '').replace(',', '.'));
+                let volume = $(el).children().eq(4).text().replace(/\n/gi, '') | '0';
 
+                // if unable to gather median price, get the lowest price
+                !price ? price = Number.parseFloat($(el).children().eq(1).text().replace(/\.|R\$ |\n/gi, '').replace(',', '.')) : null;
 
-            if (skin.minWear < floats[mw] && skin.maxWear > floats[fn])
-                skin[mw] = await queryPrice(`${skinName} (${mw})`);
-
-
-            if (skin.minWear < floats[ft] && skin.maxWear > floats[mw])
-                skin[ft] = await queryPrice(`${skinName} (${ft})`);
-
-
-            if (skin.minWear < floats[ww] && skin.maxWear > floats[ft])
-                skin[ww] = await queryPrice(`${skinName} (${ww})`);
-
-
-            if (skin.minWear < floats[bs] && skin.maxWear > floats[ww])
-                skin[bs] = await queryPrice(`${skinName} (${bs})`);
-
-
-            // writes in disk
-            fs.writeFile(`./data/skins/${skinName}.json`, JSON.stringify(skin), err => {
-                if (err) {
-                    reject(null);
-                    return console.warn('\x1b[31m%s\x1b[0m', "Erro ao atualizar a skin!\n", err);
+                if (quality.includes('Factory New')) {
+                    if (isST)
+                        skin["stat-track"].FN = {
+                            price,
+                            volume
+                        };
+                    else
+                        skin.normal.FN = {
+                            price,
+                            volume
+                        };
+                } else if (quality.includes('Minimal Wear')) {
+                    if (isST)
+                        skin["stat-track"].MW = {
+                            price,
+                            volume
+                        };
+                    else
+                        skin.normal.MW = {
+                            price,
+                            volume
+                        };
+                } else if (quality.includes('Field-Tested')) {
+                    if (isST)
+                        skin["stat-track"].FT = {
+                            price,
+                            volume
+                        };
+                    else
+                        skin.normal.FT = {
+                            price,
+                            volume
+                        };
+                } else if (quality.includes('Well-Worn')) {
+                    if (isST)
+                        skin["stat-track"].WW = {
+                            price,
+                            volume
+                        };
+                    else
+                        skin.normal.WW = {
+                            price,
+                            volume
+                        };
+                } else if (quality.includes('Battle-Scarred')) {
+                    if (isST)
+                        skin["stat-track"].BS = {
+                            price,
+                            volume
+                        };
+                    else
+                        skin.normal.BS = {
+                            price,
+                            volume
+                        };
+                } else {
+                    reject();
+                    return;
                 }
-
-                console.warn("\x1b[32m%s\x1b[0m", `${skinName} atualizada!`);
-                resolve(true);
             });
 
-        }).catch(res => {
-            reject(null);
-            console.warn('\x1b[31m%s\x1b[0m', "Erro ao pegar informacao da caixa!\n");
+            fs.writeFile(`./data/skins/${skinName.replace('|','!')}.json`, JSON.stringify(skin), err => {
+                if (err) {
+                    reject();
+                    return console.warn(COLOR.RED, `Error updating ${skinName}!`, err);
+                }
 
+                console.warn(COLOR.GREEN, `${skinName} updated successfuly!`)
+                resolve(skin);
+            });
+        }).catch(err => {
+            reject();
+            console.warn(COLOR.RED, `Error updating ${skinName}!`, err);
         });
-
     });
 }
 
-function routineUpdateWholeCase(caseName, caseLink) {
+function updateCase(caseName, caseLink) {
     return new Promise((resolve, reject) => {
-        updateCaseSkins(caseName, caseLink).then(async res => {
-            // updates skins from all qualities
-            for(const qual of Object.keys(res))
-                for(const skinName of Object.keys(res[qual]))
-                    await updateSkin(skinName, res[qual][skinName]);
+        const c = { // case
+            name: caseName,
+            link: caseLink,
+            skins: {}
+        };
 
-            resolve(null);
+        axios.get(caseLink, {timeout: 2000}).then(res => {
+            const $ = cheerio.load(res.data);
+
+            $('.well.result-box.nomargin') // gets all items
+                .not($('.quality.color-rare-item').parent()) // remove knives
+                .not($('.abbu-body-resultbox').parent()) // remove ads
+                .each((i, el) => {
+                    if (!c.skins[$(el).find('.quality').attr('class').slice(14, )])
+                        c.skins[$(el).find('.quality').attr('class').slice(14, )] = []
+                    c.skins[$(el).find('.quality').attr('class').slice(14, )].push({
+                        skinName: $(el).find('h3').text(),
+                        skinLink: $(el).children('a').not('.nounderline').attr('href')
+                    })
+                });
+
+            fs.writeFile(`./data/${caseName}.json`, JSON.stringify(c), err => {
+                if (err) {
+                    reject();
+                    return console.warn(COLOR.RED, `Error updating ${caseName}!`, err);
+                }
+
+                console.warn(COLOR.GREEN, `\n* ${caseName} updated successfuly!`);
+                resolve(c);
+            });
 
         }).catch(err => {
-            reject(null);
-            console.warn('\x1b[31m%s\x1b[0m', "Erro na requisicao da caixa!\n", err);
-
+            reject();
+            console.warn(COLOR.RED, `Error updating ${caseName}!`, err);
         });
     });
 }
 
-function routineUpdateWholeDB() {
+function updateAllCases() {
     return new Promise((resolve, reject) => {
-        updateCases().then(async res => {
-            for(const caseName of Object.keys(res))  
-                await routineUpdateWholeCase(caseName, res[caseName])
+        const caseList = [];
+
+        axios.get(`https://csgostash.com/containers/skin-cases`, {timeout: 2000}).then(res => {
+            const $ = cheerio.load(res.data);
+
+            // creates an object with all the cases names and links
+            $('.well.result-box.nomargin') // get all cases
+                .not($('.abbu-body-resultbox').parent()) // removes ads
+                .each((i, el) => {
+                    caseList.push({
+                        caseName: $(el).find('h4').text(),
+                        caseLink: $(el).find('a').attr('href')
+                    });
+                });
+
+            fs.writeFile("./data/caseList.json", JSON.stringify(caseList), (err) => {
+                if (err) {
+                    reject();
+                    return console.warn(COLOR.RED, 'Error updating Case list!', err);
+                }
+
+                console.warn(COLOR.GREEN, 'Case list successfuly updated!');
+                resolve(caseList);
+            });
+
+        }).catch(err => {
+            reject();
+            console.warn(COLOR.RED, `Error updating Case list!`, err);
+        });
+    });
+
+}
+
+function syncCase(caseName, caseLink, tries = 5, ms = 10000) {
+    if (tries <= 0)
+        return Promise.reject(`Maximum tries on ${caseName}`);
+
+    return updateCase(caseName, caseLink).then(async caseInfo =>{
+        for (const quality in caseInfo.skins) { // forEach quality in the case
+            for (const skin of caseInfo.skins[quality]) { // forEach skin in the quality
+                await syncSkin(skin.skinName, skin.skinLink);
+            }
+        }
+    }).catch(async _ => {
+        await delay(ms);
+        return syncCase(skinName, skinLink, tries - 1);
+    });
+}
+
+function syncSkin(skinName, skinLink, tries = 5, ms = 10000) {
+    if (tries <= 0)
+        return Promise.reject(`Maximum tries on ${skinName}`);
+
+    return updateSkin(skinName, skinLink).catch(async _ => {
+        await delay(ms);
+        return syncSkin(skinName, skinLink, tries - 1);
+    });
+}
+
+function routineSyncWholeDB() {
+    return new Promise(async resolve => {
+        await updateAllCases().then(async caseList => {
+            for (const c of caseList) { // forEach case in the list
+                await syncCase(c.caseName, c.caseLink);
+            }
 
             resolve();
-
-        }).catch(err => {
-            reject(null);
-            console.warn('\x1b[31m%s\x1b[0m', "Erro na requisicao das caixas!\n", err);
-
         });
     });
 }
 
-routineUpdateWholeDB().then(res => console.log("Acabou!"));
+routineSyncWholeDB();
